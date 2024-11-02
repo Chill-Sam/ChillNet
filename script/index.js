6ad2ebd81f713b4f3af0fcdc2a2b2e744c56450d
Jonathan Wahrenberg <jonathan@wahrenberg.com>
2024-11-02 15:31:15 +0000

    refactored more js

--- script/index.js
+++ script/index.js
@@ -5,23 +5,6 @@ function showNoAccount(show) {
     enableChildren(popupContainer, show);
 }
 
-function clearLoginErrors() {
-    const popupContainer = "#loginContainer";
-    $(popupContainer)
-        .find("label")
-        .each(function () {
-            $(this).empty();
-        });
-}
-
-function showLogin(show) {
-    const popupContainer = "#loginContainer";
-    setOpacity(popupContainer, show);
-    setPointerEvents(popupContainer, show);
-    enableChildren(popupContainer, show);
-    clearLoginErrors();
-}
-
 function showAccount(show) {
     const accountPopup = "#accountPopup";
     setOpacity(accountPopup, show);
@@ -52,9 +35,9 @@ $(document).ready(function () {
             const accountPopup = document.getElementById("accountPopup");
             const profileButton = document.getElementById("profileButton");
 
-            var pressingPopup = accountPopup.contains(event.target);
-            var pressingProfile = profileButton.contains(event.target);
-            var isVisible = accountPopup.style.opacity == 1;
+            let pressingPopup = accountPopup?.contains(event.target);
+            let pressingProfile = profileButton?.contains(event.target);
+            let isVisible = accountPopup?.style.opacity == 1;
 
             if (!pressingPopup && !pressingProfile && isVisible) {
                 showAccount(false);
@@ -70,41 +53,6 @@ $(document).ready(function () {
         if (postContent.trim() !== "") {
             this.submit();
         }
-    });
-
-    $("#loginForm").submit(function (e) {
-        e.preventDefault();
-
-        const form = $(this);
-
-        $.ajax({
-            type: "POST",
-            url: "php/loginUser",
-            data: form.serialize(), // serializes the form's elements.
-            success: function (data) {
-                const usernameError = $("#usernameError");
-                const passwordError = $("#passwordError");
-
-                const noAccountError = "Account does not exist";
-                const wrongPasswordError = "Incorrect password";
-
-                switch (data) {
-                    case "NA":
-                        usernameError.html(noAccountError);
-                        passwordError.empty();
-                        break;
-                    case "WP":
-                        usernameError.empty();
-                        passwordError.html(wrongPasswordError);
-                        break;
-                    default:
-                        usernameError.empty();
-                        passwordError.empty();
-                        location.reload();
-                        break;
-                }
-            },
-        });
     });
 });
 
