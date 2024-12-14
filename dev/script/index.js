b622c442ffd2fb8abe576970caea961faeac7a9a
Jonathan Wahrenberg <jonathan@wahrenberg.com>
2024-12-14 19:38:49 +0000

    Added dev dir with experimental layout

---
+++ dev/script/index.js
@@ -0,0 +1,109 @@
+let userCache = {};
+
+function toggleMenu() {
+    const menu = $(".menu");
+    const cover = $(".cover");
+    menu.toggleClass("visible");
+    cover.toggleClass("on");
+}
+
+async function fetchUserDetails(UserId) {
+    if (userCache[UserId] !== undefined) {
+        return Promise.resolve(userCache[UserId]);
+    } else {
+        return $.ajax({
+            url: "/api/getUser",
+            method: "GET",
+            data: { UserId: UserId },
+            dataType: "json",
+        }).then((response) => {
+            userCache[UserId] = response.data;
+            return response.data;
+        });
+    }
+}
+
+async function handleNewPosts(posts) {
+    for (const post of posts) {
+        const user = await fetchUserDetails(post.AssUserId);
+        const date = new Date(post.PostDate + "Z").toLocaleDateString();
+        const time = new Date(post.PostDate + "Z").toLocaleTimeString("en-US", {hour12: false});
+        $("#posts-container").append(`
+            <div class="post">
+                <div class="post-header">
+                    <h1>${user.Username}</h1>
+                    <div class="post-time">
+                        <h2>${date}</h2>
+                        <h4>${time}</h4>
+                    </div>
+                </div>
+                <p>${post.Content}</p>
+            </div>
+            `);
+    }
+}
+
+$("#navbar-toggle").on("click", function () {
+    toggleMenu();
+});
+
+$("#menu-close").on("click", function () {
+    toggleMenu();
+});
+
+$("#menu-settings").on("click", function () {});
+
+$("#menu-about").on("click", function () {});
+
+$("#menu-github").on("click", function () {
+    window.location.replace("https://github.com/Chill-Sam/ChillNet");
+});
+
+$("#menu-logout").on("click", async function () {
+    await $.ajax({
+        type: "POST",
+        url: "/api/logout",
+    });
+    window.location.replace("/signin");
+});
+
+$(".cover").on("click", function (e) {
+    if (!$(this).hasClass("on")) {
+        return;
+    }
+
+    if (!$(e.target).is(".menu")) {
+        toggleMenu();
+    }
+});
+
+$(document).ready(function () {
+    if (!(SESSION_USERID === "")) {
+        (async () => {
+            const curUser = await fetchUserDetails(Number(SESSION_USERID));
+            $(".navbar-message").append(curUser.Username);
+        })();
+    }
+
+    const postsSocket = new WebSocket(
+        "ws://chillsam.ddns.net:8080/websocket/posts",
+    );
+
+    postsSocket.onopen = function () {};
+
+    postsSocket.onmessage = function (event) {
+        const message = JSON.parse(event.data);
+
+        switch (message.type) {
+            case "latest_posts":
+                handleNewPosts(message.data);
+                break;
+            default:
+                break;
+        }
+    };
+
+    postsSocket.onclose = function () {};
+
+    postsSocket.onerror = function (error) {};
+});
