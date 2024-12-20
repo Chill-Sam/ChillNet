710ee16a25e258adbea1e804acbf10d8d7bc4d1d
Jonathan Wahrenberg <jonathan@wahrenberg.com>
2024-12-20 18:55:21 +0000

    Added functionality to get older posts

--- websockets/postWebsocket.php
+++ websockets/postWebsocket.php
@@ -25,6 +25,64 @@ class PostWebSocket implements MessageComponentInterface
     {
         echo "New connection to PostWebSocket: {$conn->resourceId}\n";
 
+        $posts = $this->getLatestPosts();
+
+        // Send the latest 15 posts to the client
+        $conn->send(json_encode([
+            'type' => 'latest_posts',
+            'data' => $posts
+        ]));
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        echo "Message recieved: {$msg}\n";
+
+        $data = json_decode($msg, true);
+
+        if (isset($data['action'])) {
+            switch ($data['action']) {
+                case 'load_initial':
+                    $response = [
+                        'type' => 'latest_posts',
+                        'data' => $this->getLatestPosts()
+                    ];
+                    break;
+
+                case 'load_more_posts':
+                    if (!isset($data['index'])) {
+                        $response = [
+                            'error' => 'No index provided',
+                        ];
+                        break;
+                    }
+                    $response = [
+                        'type' => 'older_posts',
+                        'index' => $data['index'],
+                        'data' => $this->getMorePosts($data['index']),
+                    ];
+                    break;
+            }
+
+            $from->send(json_encode($response));
+        } else {
+            $from->send(json_encode(['error' => 'Invalid request']));
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        echo "Connection to PostWebSocket {$conn->resourceId} closed\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "Error on PostWebSocket: {$e->getMessage()}\n";
+        $conn->close();
+    }
+
+    private function getLatestPosts()
+    {
         if (!$this->db->ping()) {
             $this->reconnectDatabase();
         }
@@ -39,23 +97,31 @@ class PostWebSocket implements MessageComponentInterface
             }
         }
 
-        // Send the latest 15 posts to the client
-        $conn->send(json_encode([
-            'type' => 'latest_posts',
-            'data' => $posts
-        ]));
+        return $posts;
     }
 
-    public function onMessage(ConnectionInterface $from, $msg) {}
-
-    public function onClose(ConnectionInterface $conn)
+    private function getMorePosts($index)
     {
-        echo "Connection to PostWebSocket {$conn->resourceId} closed\n";
-    }
+        $posts = [];
+        $query = 'SELECT * FROM Posts ORDER BY PostDate DESC LIMIT 15 OFFSET ?';
 
-    public function onError(ConnectionInterface $conn, \Exception $e)
-    {
-        echo "Error on PostWebSocket: {$e->getMessage()}\n";
-        $conn->close();
+        if ($stmt = $this->db->prepare($query)) {
+            $stmt->bind_param('i', $index);
+
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+            if ($result && $result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $posts[] = $row;  // Collect posts in an array
+                }
+            }
+
+            $stmt->close();
+        } else {
+            error_log('Database query error: ' . $this->db->error);
+        }
+
+        return $posts;
     }
 }
