fc17780092fa04cf7706e6a050d4ca9a45848cac
Jonathan Wahrenberg <jonathan@wahrenberg.com>
2025-01-07 13:08:19 +0000

    Added 'throbber' to load new posts

--- dev/script/index.js
+++ dev/script/index.js
@@ -1,6 +1,7 @@
 let userCache = {};
 let count = 0;
 let isLoadingPosts = false;
+let loadedAllPosts = false;
 
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
@@ -28,6 +29,14 @@ async function fetchUserDetails(UserId) {
 }
 
 async function handleNewPosts(posts) {
+    if (posts.length == 0) {
+        loadedAllPosts = true;
+        $("#posts").append(`
+            <div class="post" style="display:flex;align-items:center;justify-content:center;"><h1>No more posts</h1></div>
+            `);
+        $("#loading")[0].style.display = "none";
+        return;
+    }
     for (const post of posts) {
         count++;
         const user = await fetchUserDetails(post.AssUserId);
@@ -35,7 +44,7 @@ async function handleNewPosts(posts) {
         const time = new Date(post.PostDate + "Z").toLocaleTimeString("en-US", {
             hour12: false,
         });
-        $("#posts-container").append(`
+        $("#posts").append(`
             <div class="post">
                 <div class="post-header">
                     <h1>${user.Username}</h1>
@@ -120,25 +129,26 @@ $(document).ready(function () {
     postsSocket.onerror = function (error) {};
 
     $("#posts-container").on("scroll", async function () {
-        await delay(750);
-
         const container = $(this);
 
         if (
+            !loadedAllPosts &&
+            !isLoadingPosts &&
             container.scrollTop() + container.innerHeight() >=
-            container[0].scrollHeight
+                container[0].scrollHeight
         ) {
-            if (isLoadingPosts) {
-                return;
-            }
-
-            getMorePosts(count);
             isLoadingPosts = true;
+            container[0].style.overflowY = "hidden";
+
+            setTimeout(() => {
+                getMorePosts(count);
+                container[0].style.overflowY = "auto";
+                isLoadingPosts = false;
+            }, 1000);
         }
     });
 
     function getMorePosts(index) {
-        console.log("crazy");
         postsSocket.send(
             JSON.stringify({ action: "load_more_posts", index: index }),
         );
